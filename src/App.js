437588ab87bd967b2b71e08ccdcedb8dcfa202aa
ozge <ozge.belenn@gmail.com>
2023-03-30 16:40:59 +0300

    card yapısı oluşturuldu. Ekle butonu eklenerek yeni ürün eklenmesi sağlandı

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, SimpleGrid, List, ThemeIcon } from "@mantine/core";
+import { IconCircleCheck} from "@tabler/icons-react";
+import "./App.css";
+import Card from "./components/Card";
+import { useState } from "react";
 
-function App() {
+let storeItems = [
+  { name: "Küpe", price: 20 },
+  { name: "Bileklik", price: 15 },
+  { name: "Halhal", price: 25 },
+];
+
+const App= ()=> {
+
+  const [basketItems, setBasketItems] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <SimpleGrid cols={3} className="Padding">
+        {storeItems.map(({ name, price }) => (
+          <Card
+            key={name}
+            name={name}
+            price={price}
+            onAdd={()=> setBasketItems([...basketItems , {name}])}
+          />
+        ))}
+      </SimpleGrid>
+
+      
+      <List
+        className="Padding"
+        spacing="xs"
+        size="sm"
+        center
+        icon={
+          <ThemeIcon color="teal" size={24} radius="xl">
+            <IconCircleCheck size="1rem" />
+          </ThemeIcon>
+        }>
+          {basketItems.map(({name}, index ) =><List.Item key={index}>{name}</List.Item> )}
+        
+      </List>
+    </Container>
   );
 }
 
